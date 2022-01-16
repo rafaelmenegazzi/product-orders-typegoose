8616fc53609448fe627b8280c16f880984ecb294
rafaelmenegazzi <rafael.menegazzi.usp@gmail.com>
2022-01-16 15:35:51 -0300

    add base entity using nanoId as default _id

---
+++ src/database/base.entity.ts
@@ -0,0 +1,29 @@
+import { modelOptions, prop, Severity } from '@typegoose/typegoose';
+import { customAlphabet } from 'nanoid';
+
+const nanoid = customAlphabet('1234567890abcdef', 10);
+
+@modelOptions({
+  options: { allowMixed: Severity.ALLOW },
+  schemaOptions: {
+    timestamps: true,
+    // toJSON: {
+    //   virtuals: true,
+    //   getters: true,
+    // },
+  },
+})
+export abstract class BaseEntity {
+  @prop({
+    default: () => nanoid(),
+  })
+  _id: string;
+
+  id: string; // _id getter as string
+
+  @prop()
+  createdAt: Date; // provided by schemaOptions.timestamps
+
+  @prop()
+  updatedAt: Date; // provided by schemaOptions.timestamps
+}
